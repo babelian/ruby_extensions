b8ce055a77e3d3702b608204e5e67d6bad0b364e
Zach Powell <zach@pledgemusic.com>
2014-09-07 14:47:32 -0400

    shippable

--- Gemfile
+++ Gemfile
@@ -3,3 +3,13 @@ group :development, :test do
   gem 'rspec', '3.0.0'
   gem 'jeweler'
 end
+
+group :test do
+  if ENV['SHIPPABLE']
+    gem "ci_reporter"
+    gem "codeclimate-test-reporter", require: nil
+    gem 'minitest-reporters'
+    gem "rspec_junit_formatter"
+    gem 'simplecov-csv'
+  end
+end
--- spec/suite.rb
+++ spec/suite.rb
@@ -1,3 +1,16 @@
+require 'simplecov'
+
+if ENV['COVERAGE_REPORTS']
+  require 'simplecov-csv'
+  SimpleCov.formatter = SimpleCov::Formatter::CSVFormatter
+  SimpleCov.coverage_dir(ENV["COVERAGE_REPORTS"])
+
+  require "codeclimate-test-reporter"
+  CodeClimate::TestReporter.start
+end
+
+SimpleCov.start
+
 specs_to_run = Dir['./**/*_spec.rb']
 
 specs_to_run.each do |file|
