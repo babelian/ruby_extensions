c42f785bca253a4c131069d8eb6856971ea3e353
Zachary Powell <zach@babelian.net>
2014-09-10 20:28:19 -0400

    documentation

--- lib/ruby_extensions/nil_class_extensions.rb
+++ lib/ruby_extensions/nil_class_extensions.rb
@@ -4,9 +4,11 @@
 # does not work by module include so we're patching it directly
 class NilClass
 
-  # Add params to nil.to_s so that Rails style something.updated_at.to_s(:db) does not fail if the timestamp is nil
+  # Add params to nil.to_s so that if you call Rail's style strings on nil they won't error
+  # @example
+  #   updated_at.to_s(:db) => nil
   # @param args to be ignored
-  # @returns [String] that is blank
+  # @return [String] that is blank
   def to_s(*args)
     ''
   end
