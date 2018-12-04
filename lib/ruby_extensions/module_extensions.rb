7cd0114fb5b48c312b9084b06e8d9502481f4f52
Zach Powell <zach@babelian.net>
2018-12-04 10:42:55 -0500

    whitespace

--- lib/ruby_extensions/module_extensions.rb
+++ lib/ruby_extensions/module_extensions.rb
@@ -1,7 +1,7 @@
 module RubyExtensions
   # Module extensions
   module ModuleExtensions
-    # wrapper for {prepend} that takes a block instead of a {Module}
+    # wrapper for {prepend} that takes a block instead of a {Module}gs
     def prepend_block(*args, &block)
       if block_given?
         prepend Module.new(&block)
