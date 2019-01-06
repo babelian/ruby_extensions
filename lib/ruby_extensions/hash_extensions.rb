1260d804404b5911a88b5df8de490cdeadd4d5f5
Zach Powell <zach@babelian.net>
2019-01-06 16:29:27 -0500

    Hash#pretty_inspect and Hash#reformat

--- lib/ruby_extensions/hash_extensions.rb
+++ lib/ruby_extensions/hash_extensions.rb
@@ -13,5 +13,31 @@ module RubyExtensions
       return unless additions
       self[:class] = [self[:class], additions].compact.join(' ')
     end
+
+    def pretty_inspect
+      inspect.gsub(/:([a-z0-9_]*)=>/, '\1: ').gsub('{', '{ ').gsub('}', ' }')
+    end
+
+    # http://www.rubydoc.info/github/rubyworks/facets/Hash%3Atraverse
+    # modified to not include to_hash logic as you may want the default to_hash
+    # of an object.
+    def reformat(&block)
+      each_with_object({}) do |(k, v), h|
+        if v.is_a?(Array)
+          v.map! do |s|
+            s = yield(nil, s).last
+            s = s.reformat(&block) if s.is_a?(Hash)
+            s
+          end
+        end
+
+        v = v.reformat(&block) if v.is_a?(Hash)
+
+        nk, nv = yield(k, v)
+        h[nk] = nv
+
+        h
+      end
+    end
   end
 end
--- spec/ruby_extensions/hash_extensions_spec.rb
+++ spec/ruby_extensions/hash_extensions_spec.rb
@@ -1,9 +1,7 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe Hash do
-
   describe '#add_class' do
-
     [
       [{}, nil, {}],
       [{}, 'class1', { class: 'class1' }],
@@ -16,7 +14,41 @@ describe Hash do
         expect(input).to eql(output)
       end
     end
-
   end
 
+  describe '#pretty_inspect' do
+    [
+      [{ a: 'b' }, '{ a: "b" }']
+    ].each do |input, output|
+      it "#{input.inspect} => #{output}" do
+        expect(input.pretty_inspect).to eq(output)
+      end
+    end
+  end
+
+  describe '#reformat' do
+    [
+      [
+        {
+          a: {
+            b: [
+              { c: :d }
+            ]
+          }
+        },
+        {
+          'a' => {
+            'b' => [
+              { 'c' => :d }
+            ]
+          }
+        },
+        ->(k, v) { [k.is_a?(String) ? k.to_sym : k.to_s, v] }
+      ]
+    ].each do |input, output, lambda|
+      it "#{input.inspect} => #{output.inspect}" do
+        expect(input.reformat(&lambda)).to eq(output)
+      end
+    end
+  end
 end
