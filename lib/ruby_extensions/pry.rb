ffc9850c476c86d2111013f34509e02361fbda35
Zachary Powell <zach@babelian.net>
2019-10-24 13:42:29 +0100

    pry updates

--- lib/ruby_extensions/pry.rb
+++ lib/ruby_extensions/pry.rb
@@ -1,5 +1,10 @@
+# frozen_string_literal: true
 
 require 'pry'
 Pry.commands.alias_command 'c', 'continue'
 Pry.commands.alias_command 's', 'step'
 Pry.commands.alias_command 'n', 'next'
+
+Pry.config.history.should_save = true
+Pry.config.history.should_load = true
+Pry.config.editor = 'nano'
